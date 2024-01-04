c52d626f2f3552f1b2c28d9424ac450078d37143
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-01-04 13:47:06 +0200

    Update locstor-select.js

--- script/locstor-select.js
+++ script/locstor-select.js
@@ -1,40 +1,39 @@
+// your-script.js
+function SaveSelectValue(select) {
+  localStorage.setItem(select.id, select.value);
+}
 
-        function SaveSelectValue(select) {
-      localStorage.setItem(select.id, select.value);
-    }
-
-    function setSelectedOption(selectId, savedValue) {
-      const select = document.getElementById(selectId);
-      if (select) {
-        const options = select.options;
-        for (let i = 0; i < options.length; i++) {
-          if (options[i].value === savedValue) {
-            options[i].selected = true;
-            break;
-          }
-        }
+function setSelectedOption(selectId, savedValue) {
+  const select = document.getElementById(selectId);
+  if (select) {
+    const options = select.options;
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].value === savedValue) {
+        options[i].selected = true;
+        break;
       }
     }
+  }
+}
 
-    function getDynamicPrefixedId(originalId) {
-      const documentTitle = document.title.replace(/[^a-z0-9]/gi, ''); // Remove non-alphanumeric characters
-      return `${documentTitle}-${originalId}`;
-    }
+function getDynamicPrefixedId(originalId) {
+  const documentTitle = document.title.replace(/[^a-z0-9]/gi, ''); // Remove non-alphanumeric characters
+  return `${documentTitle}-${originalId}`;
+}
 
-    document.addEventListener('DOMContentLoaded', function () {
-      // Get all select elements on the page
-      const allSelects = document.querySelectorAll('select');
+document.addEventListener('DOMContentLoaded', function () {
+  // Get all select elements on the page
+  const allSelects = document.querySelectorAll('select');
 
-      // Iterate over each select element and set its selected option
-      allSelects.forEach(function (select) {
-        // Set dynamic prefixed ID for the select element
-        const originalId = select.id;
-        const dynamicPrefixedId = getDynamicPrefixedId(originalId);
-        select.id = dynamicPrefixedId;
+  // Iterate over each select element and set its selected option
+  allSelects.forEach(function (select) {
+    // Set dynamic prefixed ID for the select element
+    const originalId = select.id;
+    const dynamicPrefixedId = getDynamicPrefixedId(originalId);
+    select.id = dynamicPrefixedId;
 
-        // Get saved value from local storage and set selected option
-        const savedValue = localStorage.getItem(dynamicPrefixedId);
-        setSelectedOption(dynamicPrefixedId, savedValue);
-      });
-    });
-    
+    // Get saved value from local storage and set selected option
+    const savedValue = localStorage.getItem(dynamicPrefixedId);
+    setSelectedOption(dynamicPrefixedId, savedValue);
+  });
+});
