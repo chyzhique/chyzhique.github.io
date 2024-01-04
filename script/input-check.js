82e920f4472d1b98ac658e083695db2f3f0e3d3d
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-01-04 15:22:09 +0200

    Update input-check.js

--- script/input-check.js
+++ script/input-check.js
@@ -1,18 +1,44 @@
+// your-script.js
+function SaveInputValue(input) {
+  if (input instanceof HTMLInputElement) {
+    localStorage.setItem(input.id, input.value);
+  }
+}
+
+function setInputValue(inputId, savedValue) {
+  const input = document.getElementById(inputId);
+  if (input instanceof HTMLInputElement) {
+    input.value = savedValue;
+  }
+}
+
+function getDynamicPrefixedId(originalId) {
+  const documentTitle = document.title.replace(/[^a-z0-9]/gi, ''); // Remove non-alphanumeric characters
+  return `${documentTitle}-${originalId}`;
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-    const inputFields = document.querySelectorAll('input[type="text"]');
-    const changeDivs = document.querySelectorAll('.changeDiv');
+  // Get all input elements on the page
+  const allInputs = document.querySelectorAll('input');
 
-    inputFields.forEach((input, index) => {
-      const correctOptions = input.dataset.correctOptions.split(',').map(option => option.trim().toLowerCase());
+  // Iterate over each input element and set its value
+  allInputs.forEach(function (input) {
+    // Set dynamic prefixed ID for the input element
+    const originalId = input.id;
+    const dynamicPrefixedId = getDynamicPrefixedId(originalId);
+    input.id = dynamicPrefixedId;
 
-      input.addEventListener('input', function () {
-        const enteredWord = input.value.trim().toLowerCase();
-
-        if (correctOptions.includes(enteredWord)) {
-          changeDivs[index].style.backgroundColor = '#77DD77'; // Change background color to green if correct word is entered
-        } else {
-          changeDivs[index].style.backgroundColor = ''; // Reset background color if a different word is entered
-        }
-      });
-    });
+    // Get saved value from local storage and set input value
+    const savedValue = localStorage.getItem(dynamicPrefixedId);
+    setInputValue(dynamicPrefixedId, savedValue);
   });
+});
+
+document.addEventListener('input', function (event) {
+  // Save value when an input element changes
+  const targetElement = event.target;
+  if (targetElement instanceof HTMLInputElement) {
+    SaveInputValue(targetElement);
+  }
+});
+
