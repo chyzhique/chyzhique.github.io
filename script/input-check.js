d577354541f69cd2c714d782264c9e21595a0f4b
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-01-04 14:53:39 +0200

    Update input-check.js

--- script/input-check.js
+++ script/input-check.js
@@ -1,18 +1,21 @@
-  document.addEventListener('DOMContentLoaded', function () {
-    const inputFields = document.querySelectorAll('input[type="text"]');
-    const changeDivs = document.querySelectorAll('.changeDiv');
+document.addEventListener('DOMContentLoaded', function () {
+            const inputFields = document.querySelectorAll('input[type="text"]');
+            const changeDivs = document.querySelectorAll('.changeDiv');
 
-    inputFields.forEach((input, index) => {
-      const correctOptions = input.dataset.correctOptions.split(',').map(option => option.trim().toLowerCase());
+            inputFields.forEach((input, index) => {
+                const correctOptions = JSON.parse(input.dataset.correctOptions);
 
-      input.addEventListener('input', function () {
-        const enteredWord = input.value.trim().toLowerCase();
+                input.addEventListener('input', function () {
+                    const enteredWord = input.value.trim().toLowerCase();
 
-        if (correctOptions.includes(enteredWord)) {
-          changeDivs[index].style.backgroundColor = '#77DD77'; // Change background color to green if correct word is entered
-        } else {
-          changeDivs[index].style.backgroundColor = ''; // Reset background color if a different word is entered
-        }
-      });
-    });
-  });
+                    if (correctOptions.includes(enteredWord)) {
+                        changeDivs[index].style.backgroundColor = '#77DD77'; // Change background color to green if correct word is entered
+                    } else {
+                        changeDivs[index].style.backgroundColor = ''; // Reset background color if a different word is entered
+                    }
+
+                    // Save input value and background color
+                    SaveInputValueAndColor(input);
+                });
+            });
+        });
