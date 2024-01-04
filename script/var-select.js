3c53741b74c2b0da326381ef075d46f5def970ce
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-01-04 08:34:47 +0200

    Update var-select.js

--- script/var-select.js
+++ script/var-select.js
@@ -1,18 +1,18 @@
-    const varSpans = document.querySelectorAll('.var');
+const varSpans = document.querySelectorAll('.var');
 
-    varSpans.forEach(varSpan => {
-      const radios = varSpan.querySelectorAll('input[type="radio"]');
-      const resultMessage = varSpan;
+varSpans.forEach(varSpan => {
+  const radios = varSpan.querySelectorAll('input[type="radio"]');
+  const resultMessage = varSpan;
 
-      radios.forEach(radioButton => {
-        radioButton.addEventListener('change', function() {
-          if (radioButton.checked) {
-            if (radioButton.getAttribute('data-correct') === 'true') {
-              resultMessage.classList.add('highlight');
-            } else {
-              resultMessage.classList.remove('highlight');
-            }
-          }
-        });
-      });
+  radios.forEach(radioButton => {
+    radioButton.addEventListener('change', function() {
+      if (radioButton.checked) {
+        if (radioButton.getAttribute('data-correct') === 'true') {
+          resultMessage.style.backgroundColor = '#77DD77'; // Set your desired color here
+        } else {
+          resultMessage.style.backgroundColor = '#ff6961'; // Reset to default color if needed
+        }
+      }
     });
+  });
+});
