6430643aab26b817148ba876f263b42ddfc342d7
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-04-20 06:37:21 +0300

    Update button-select-drop.js

--- script/button-select-drop.js
+++ script/button-select-drop.js
@@ -1,29 +1,19 @@
 function checkAnswer(element) {
-    // Reset the element to default state
-    if (element.tagName === 'BUTTON') {
-        let buttons = element.parentElement.getElementsByTagName('button');
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].classList.remove('correct', 'incorrect');
-        }
+    // Reset the background color of the select element
+    element.classList.remove('correct', 'incorrect');
 
-        // Check if the selected answer is correct
-        let correctAnswer = element.parentElement.getAttribute('data-correct');
-        if (element.id === correctAnswer) {
-            element.classList.add('correct');
-        } else {
-            element.classList.add('incorrect');
-        }
-    } else if (element.tagName === 'SELECT') {
-        // Reset the background color of the select element
-        element.classList.remove('correct', 'incorrect');
-
-        // Check if the selected answer is correct
-        const correctAnswer = element.parentElement.getAttribute('data-correct');
-        const selectedOption = element.options[element.selectedIndex];
-        if (selectedOption.value === correctAnswer) {
-            element.classList.add('correct');
-        } else {
-            element.classList.add('incorrect');
-        }
+    // Check if the selected answer is correct
+    const correctAnswer = element.parentElement.getAttribute('data-correct');
+    const selectedOption = element.options[element.selectedIndex];
+    if (selectedOption.value === correctAnswer) {
+      element.classList.add('correct');
+    } else {
+      element.classList.add('incorrect');
     }
-}
+  }
+
+  document.querySelectorAll('select').forEach(select => {
+    select.addEventListener('change', function() {
+      checkAnswer(this);
+    });
+  });
