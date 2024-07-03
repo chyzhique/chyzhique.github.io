faf89bbde6be5b58a666947c6250c397f05c6d10
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-07-03 12:53:45 +0300

    Update navbar.js

--- site/scripts/navbar.js
+++ site/scripts/navbar.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navbarHTML = `
           <div id="navbar" class="navbar">
-  <a href="#">Profile</a>
-  <a href="#">Courses</a>
-  <a href="#">Current lesson</a>
-  <a href="#">My Materials</a>
-  <a href="#" class="right">Pricing</a>
+  <a href="https://chyzhique.github.io/site/index.html">Main</a>
+  <a href="https://chyzhique.github.io/site/courses.html">Courses</a>
+  <a href="https://chyzhique.github.io/site/lesson.html">Current lesson</a>
+  <a href="https://chyzhique.github.io/site/materials.html">My Materials</a>
+  <a href="https://chyzhique.github.io/site/pricing.html" class="right">Pricing</a>
 </div>
     `;
     document.body.insertAdjacentHTML('afterbegin', navbarHTML);
