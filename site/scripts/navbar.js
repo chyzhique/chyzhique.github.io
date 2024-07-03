8bcd14df53f7c27cd7f0059bf0fee02fda24fbf3
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-07-03 10:34:54 +0300

    Update navbar.js

--- site/scripts/navbar.js
+++ site/scripts/navbar.js
@@ -1,9 +1,12 @@
-function loadNavbar() {
-            fetch('https://chyzhique.github.io/site/navbar.html')
-                .then(response => response.text())
-                .then(data => {
-                    document.getElementById('navbar').innerHTML = data;
-                });
-        }
-
-        window.onload = loadNavbar;
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarHTML = `
+          <div id="navbar" class="navbar">
+  <a href="#">Profile</a>
+  <a href="#">Courses</a>
+  <a href="#">Current lesson</a>
+  <a href="#">My Materials</a>
+  <a href="#" class="right">Pricing</a>
+</div>
+    `;
+    document.body.insertAdjacentHTML('afterbegin', navbarHTML);
+});
