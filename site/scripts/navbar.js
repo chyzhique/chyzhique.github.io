55e1ad0a397f7f4e2b2c5a3f8342b30979786d93
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-07-06 21:53:36 +0300

    Update navbar.js

--- site/scripts/navbar.js
+++ site/scripts/navbar.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navbarHTML = `
           <div id="navbar" class="navbar">
+          <span class="logo"><img class="flavicon" src="https://www.dropbox.com/scl/fi/a0qiv80qrjzu8vvvv2ouk/512sign.png?rlkey=ykjui910b9eufvs1n0yzbok2s&st=m0y64ssi&raw=1">ANGLOMOVA</span>
   <a href="https://chyzhique.github.io/site/index.html">Main</a>
   <a href="https://chyzhique.github.io/site/courses.html">Courses</a>
   <a href="https://chyzhique.github.io/site/lesson.html">Current lesson</a>
