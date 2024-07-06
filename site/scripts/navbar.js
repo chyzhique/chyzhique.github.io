281cf2912aef892ae12f4d77447aeb1e7ecb7298
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-07-06 22:00:53 +0300

    Update navbar.js

--- site/scripts/navbar.js
+++ site/scripts/navbar.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navbarHTML = `
+    <head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    </head>
           <div id="navbar" class="navbar">
           <span class="logo"><img class="flavicon" src="https://www.dropbox.com/scl/fi/a0qiv80qrjzu8vvvv2ouk/512sign.png?rlkey=ykjui910b9eufvs1n0yzbok2s&st=m0y64ssi&raw=1">ANGLOMOVA</span>
-  <a href="https://chyzhique.github.io/site/index.html">Main</a>
-  <a href="https://chyzhique.github.io/site/courses.html">Courses</a>
-  <a href="https://chyzhique.github.io/site/lesson.html">Current lesson</a>
-  <a href="https://chyzhique.github.io/site/materials.html">My Materials</a>
-  <a href="https://chyzhique.github.io/site/pricing.html" class="right">Pricing</a>
+  <a href="https://chyzhique.github.io/site/index.html"><span class="material-symbols-outlined">home</span>Main</a>
+  <a href="https://chyzhique.github.io/site/courses.html"><span class="material-symbols-outlined">dataset</span>Courses</a>
+  <a href="https://chyzhique.github.io/site/lesson.html"><span class="material-symbols-outlined">bolt</span>Current lesson</a>
+  <a href="https://chyzhique.github.io/site/materials.html"><span class="material-symbols-outlined">save</span>My Materials</a>
+  <a href="https://chyzhique.github.io/site/profile.html" class="right"><span class="material-symbols-outlined">account_circle</span>Profile</a>
 </div>
     `;
     document.body.insertAdjacentHTML('afterbegin', navbarHTML);
