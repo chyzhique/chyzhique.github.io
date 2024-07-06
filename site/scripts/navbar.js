86e31871f92570e4586f84c134b0d07c4c87a874
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-07-07 02:53:24 +0300

    Update navbar.js

--- site/scripts/navbar.js
+++ site/scripts/navbar.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navbarHTML = `
           <div id="navbar" class="navbar">
-          <span class="logo"><img class="icon" src="https://www.dropbox.com/scl/fi/a0qiv80qrjzu8vvvv2ouk/512sign.png?rlkey=ykjui910b9eufvs1n0yzbok2s&st=m0y64ssi&raw=1">ANGLOMOVA</span>
+  <a><span class="logo"><img class="icon" src="https://www.dropbox.com/scl/fi/a0qiv80qrjzu8vvvv2ouk/512sign.png?rlkey=ykjui910b9eufvs1n0yzbok2s&st=m0y64ssi&raw=1">ANGLOMOVA</span>
   <a href="https://chyzhique.github.io/site/index.html"><span class="material-symbols-outlined">home</span>Main</a>
   <a href="https://chyzhique.github.io/site/courses.html"><span class="material-symbols-outlined">dataset</span>Courses</a>
   <a href="https://chyzhique.github.io/site/lesson.html"><span class="material-symbols-outlined">bolt</span>Current lesson</a>
