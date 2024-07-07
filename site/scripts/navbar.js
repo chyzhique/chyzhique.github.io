f16c68919177e0d5df6f0527797159ccc9df629f
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-07-08 00:29:51 +0300

    Update navbar.js

--- site/scripts/navbar.js
+++ site/scripts/navbar.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navbarHTML = `
-    <div class="nav-root">
-    <div class="wire_cont">
-      <div class="wire_cont2">
+    <div class="navroot">
+    <div class="wire-cont">
+      <div class="wire-cont2">
         <div style="--gv-text-contextual-dark: rgba(255, 255, 255, 1); --gv-text-contextual-base: rgba(255, 255, 255, 0.9); --gv-text-contextual-pale: rgba(255, 255, 255, 0.8); --gv-text-contextual-xpale: rgba(255, 255, 255, 0.7); --gv-text-contextual-disabled: rgba(255, 255, 255, 0.6); --gv-bg-contextual-disabled: rgba(255, 255, 255, 0.05); --gv-text-contextual-inverse: #0D0D0D; --gv-text-contextual-accent: rgba(255, 255, 255, 1); --gv-bg-container-highlight: rgba(255, 255, 255, 0.05); --gv-bg-hovered: rgba(255, 255, 255, 0.1); --gv-border-dark: rgba(255,255,255,0.5); --gv-border-base: rgba(255, 255, 255, 0.15);">
           
     <header class="desktop-nav-bar" id="navbar">
