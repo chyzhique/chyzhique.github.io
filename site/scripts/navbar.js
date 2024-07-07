4dd425266a8c12482fea370fcb5ebd405c7991cd
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-07-07 04:05:37 +0300

    Update navbar.js

--- site/scripts/navbar.js
+++ site/scripts/navbar.js
@@ -1,13 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navbarHTML = `
-          <div id="navbar" class="navbar">
-  <a><span class="logo"><img class="icon" src="https://www.dropbox.com/scl/fi/a0qiv80qrjzu8vvvv2ouk/512sign.png?rlkey=ykjui910b9eufvs1n0yzbok2s&st=m0y64ssi&raw=1">ANGLOMOVA</span>
-  <a href="https://chyzhique.github.io/site/index.html"><span class="material-symbols-outlined">home</span>Main</a>
-  <a href="https://chyzhique.github.io/site/courses.html"><span class="material-symbols-outlined">dataset</span>Courses</a>
-  <a href="https://chyzhique.github.io/site/lesson.html"><span class="material-symbols-outlined">bolt</span>Current lesson</a>
-  <a href="https://chyzhique.github.io/site/materials.html"><span class="material-symbols-outlined">save</span>My Materials</a>
-  <a href="https://chyzhique.github.io/site/profile.html" class="right"><span class="material-symbols-outlined">account_circle</span>Profile</a>
+    <div class="nav-root">
+    <div class="nav-wire">
+    <div class="desktop-nav-bar" id="navbar">
+    <div class="unified-nav">
+<img class="logo" src="https://www.dropbox.com/scl/fi/a0qiv80qrjzu8vvvv2ouk/512sign.png?rlkey=ykjui910b9eufvs1n0yzbok2s&st=m0y64ssi&raw=1"></div>
+<span class="company">ANGLOMOVA</span>
 </div>
+  <nav class="objectives">
+  
+  <button type="button" class="navbut" onclick="document.location='https://chyzhique.github.io/site/index.html'">
+  <div class="piclog" size="20"><span class="material-symbols-outlined">home</span></div><div>Main</div></button>
+  
+  <button type="button" class="navbut" onclick="document.location='https://chyzhique.github.io/site/courses.html'">
+  <div class="piclog" size="20"><span class="material-symbols-outlined">dataset</span></div><div>Courses</div></button>
+  
+  <button type="button" class="navbut" onclick="document.location='https://chyzhique.github.io/site/lesson.html'">
+  <div class="piclog" size="20"><span class="material-symbols-outlined">bolt</span></div><div>Current lesson</div></button>
+  
+  <button type="button" class="navbut" onclick="document.location='https://chyzhique.github.io/site/materials.html'">
+  <div class="piclog" size="20"><span class="material-symbols-outlined">save</span></div><div>Materials</div></button>
+  </nav>
+  <div style="align-items: center; box-sizing: border-box;display: flex;;">
+  <button type="button" class="navbut" onclick="document.location='https://chyzhique.github.io/site/profile.html'">
+  <div class="piclog" size="20"><span class="material-symbols-outlined">account_circle</span></div><div>Profile</div></button>
+  
+</div></div></div>
     `;
     document.body.insertAdjacentHTML('afterbegin', navbarHTML);
 });
