9dd32bfd3c612d9f231801edfc70c331228506f0
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-07-11 22:39:35 +0300

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  // Your web app's Firebase configuration here
+  // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
+  apiKey: "AIzaSyA9fv5y23398kzP8HljS3BlreOjycA-NUI",
+  authDomain: "anglomova-429e2.firebaseapp.com",
+  databaseURL: "https://anglomova-429e2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "anglomova-429e2",
+  storageBucket: "anglomova-429e2.appspot.com",
+  messagingSenderId: "30552534006",
+  appId: "1:30552534006:web:74d0515d25b5778e30f5d7",
+  measurementId: "G-9W633V0TWL"
+};
