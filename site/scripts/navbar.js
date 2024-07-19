b1b1a90476b3072d5c61e886eb08e125a151f989
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-07-19 07:58:58 +0300

    Update navbar.js

--- site/scripts/navbar.js
+++ site/scripts/navbar.js
@@ -1,45 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navbarHTML = `
-    <div class="navroot">
-    <div class="wire-cont">
-      <div class="wire-cont2">
-        <div style="--gv-text-contextual-dark: rgba(255, 255, 255, 1); --gv-text-contextual-base: rgba(255, 255, 255, 0.9); --gv-text-contextual-pale: rgba(255, 255, 255, 0.8); --gv-text-contextual-xpale: rgba(255, 255, 255, 0.7); --gv-text-contextual-disabled: rgba(255, 255, 255, 0.6); --gv-bg-contextual-disabled: rgba(255, 255, 255, 0.05); --gv-text-contextual-inverse: #0D0D0D; --gv-text-contextual-accent: rgba(255, 255, 255, 1); --gv-bg-container-highlight: rgba(255, 255, 255, 0.05); --gv-bg-hovered: rgba(255, 255, 255, 0.1); --gv-border-dark: rgba(255,255,255,0.5); --gv-border-base: rgba(255, 255, 255, 0.15);">
-          
-    <header class="desktop-nav-bar" id="navbar">
-      <div class="desktop-nav-bar-lef">
-    <div class="unified-nav">
-<img class="logo" src="https://www.dropbox.com/scl/fi/a0qiv80qrjzu8vvvv2ouk/512sign.png?rlkey=ykjui910b9eufvs1n0yzbok2s&st=m0y64ssi&raw=1"></div>
-<span class="company">ANGLOMOVA</span>
-</div>
-  <nav class="objectives">
-  
-  <button type="button" class="navbut" onclick="document.location='/index.html'">
-  <div class="piclog" size="20"><span class="material-symbols-outlined">home</span></div><div>Main</div></button>
-  
-  <button type="button" class="navbut" onclick="document.location='/courses.html'">
-  <div class="piclog" size="20"><span class="material-symbols-outlined">dataset</span></div><div>Courses</div></button>
-  
-  <button type="button" class="navbut" onclick="document.location='/lesson.html'">
-  <div class="piclog" size="20"><span class="material-symbols-outlined">bolt</span></div><div>Current lesson</div></button>
-  
-  <button type="button" class="navbut" onclick="document.location='/materials.html'">
-  <div class="piclog" size="20"><span class="material-symbols-outlined">save</span></div><div>Materials</div></button>
-
-  <button type="button" class="navbut" onclick="document.location='/pricing.html'">
-  <div class="piclog" size="20"><span class="material-symbols-outlined">paid</span></div><div>Pricing</div></button>
-  </nav>
-      
-  <div class="nav-bar-rig">
-  <button type="button" class="navbut-prof" onclick="document.location='/profile.html'">
-<div class="ppr">
-    <span class="material-symbols-outlined">account_circle</span></div><div></div></button>
-    </header>
-  
-</div>
+<div class="root">
+<div class="navbar">
+  <div class="nav-left">
+    <img
+      src="https://anglomova.com/site/images/512sign.png"
+      class="logo"
+    />
+    <div class="nav-title">Anglomova</div>
   </div>
-    </div>
+  <div class="nav-panel">
+    <div class="nav-panel-element" onclick="document.location='/index.html'">
+        <span class="material-symbols-outlined">home</span>
+        <span>Main</span>
       </div>
+      <div class="nav-panel-element" onclick="document.location='/courses.html'">
+        <span class="material-symbols-outlined">book</span>
+        <span>Courses</span>
+      </div>
+      <div class="nav-panel-element" onclick="document.location='/lesson.html'">
+        <span class="material-symbols-outlined">bolt</span>
+        <span>Current lesson</span>
+      </div>
+      <div class="nav-panel-element" onclick="document.location='/materials.html'">
+        <span class="material-symbols-outlined">star</span>
+        <span>My materials</span>
+      </div>
+      <div class="nav-panel-element" onclick="document.location='/pricing.html'">
+        <span class="material-symbols-outlined">paid</span>
+        <span>Pricing</span>
+      </div>
+    </div>
+  <div class="nav-auth">
+    <button class="nav-auth-sign" onclick="document.location='/login.html'">Sign in</button>
+    <button class="nav-auth-reg" onclick="document.location='/login.html'">Register</button>
   </div>
+</div>
     `;
     document.body.insertAdjacentHTML('afterbegin', navbarHTML);
 });
