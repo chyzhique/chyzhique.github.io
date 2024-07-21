f72ca894f1a27b4b3416fdd54f81f60d0fa15753
Anglomova <126631488+chyzhique@users.noreply.github.com>
2024-07-21 04:36:42 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Navbar />
+    <header className="App-header">
+      <h1>Welcome to My React App</h1>
+    </header>
+  </div>
+);
+
+export default App;
